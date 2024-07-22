bef558d1e2984a84f3e2b131687c197e58467f88
Laila Leal <135389321+lailaleal@users.noreply.github.com>
2024-07-21 22:26:43 -0300

    Add files via upload

---
+++ Primo rapido.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+bool eprimo(int num){
+    if (num <=1) return false;
+    if (num == 2) return true;
+    if (num%2==0) return false;
+    for (int i=3; i <= sqrt(num); i+=2){
+        if (num % i == 0) return false;
+    } return true;
+
+}
+int main (){
+    int N, X;
+    cin >> N;
+    for (int i=0; i<N; i++){
+        cin >> X;
+        if (eprimo(X)){
+            cout  <<"Prime" << endl;
+        }
+        else{ cout << "Not Prime" << endl;}
+    }
+
+return 0;}
